106ec8e93eebc074feabfc6b5ea7bf1e15a58859
AlexeiKarpachBY <alexeikarpach@gmail.com>
2021-01-16 23:15:29 +0300

    Final

--- src/IOC/Main.java
+++ src/IOC/Main.java
@@ -1,6 +1,13 @@
 package IOC;
 
 public class Main {
-    float[] floatArray = {2.0235F, 8.458F, 5.98F, 1.999F};
-    String[] stringArray = {"a235bd", "45cxbh8", "re9vb8w", "999f"};
+    public static void main(String[] args) {
+        float[] floatArray = {2.0235F, 8.458F, 5.98F, 1.999F};
+        String[] stringArray = {"a235bd", "45cxbh8", "re9vb8w", "999f"};
+
+        System.out.println(Alexei.comparisonOfTwoIntArrays(floatArray, stringArray));
+    }
+
+
+
 }
